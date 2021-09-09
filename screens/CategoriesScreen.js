57f38706511c516fd5affcdb22c9865d332aab23
Christian <tiradoc02@gmail.com>
2021-09-09 15:36:07 -0400

    A bunch of Changes :)

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,28 +1,57 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from "react-native";
+import React from "react";
+import { StyleSheet, FlatList, View, Text, TouchableOpacity, Platform } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
+
 const CategoriesScreen = (props) => {
-    
-    return (
-        <View style={styles.screen}>
-            <Text>The Categories Screen</Text>
-            <Button title="Go to Meals!" onPress={() => {
-                
-                props.navigation.navigate('CategoryMeals');
-                //props.navigation.push('Categories');
-                // push allows you render the same screen with different content.
-            
-                //props.navigation.replace('CategoryMeals'); replace the current element in the stack.
-            }} />
-        </View>
-    );
+
+    const renderGridItem = ({ item }) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals', params: {
+                    categoryId: item.id
+                }});
+            }}>
+             <View>
+             <Text>{item.title}</Text>
+             </View>
+          </TouchableOpacity>
+          
+        );
+      };
+
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={CATEGORIES}
+        renderItem={renderGridItem}
+        numColumns={2}
+      />
+    </View>
+  );
 };
 
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ?  Colors.primaryColor : 'white'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+}
+
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  
+  gridItem: {
+      flex: 1,
+      margin: 15,
+      height: 150,
+  }
 });
 
 export default CategoriesScreen;
